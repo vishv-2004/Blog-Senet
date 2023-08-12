bd70acedc7f2a9db8a5a550c476f2cb188fdeb12
Vishv Patel <vishvp.streamspace@gmail.com>
2023-08-12 21:54:04 +0530

    initial commit

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const { createBlog } = require("../controller/blog");
+const { createBlog, getBlogs } = require("../controller/blog");
 const { auth } = require("../middleware/auth");
-
 const Routes = express.Router();
 
 Routes.post("/createblog",auth,createBlog);
-
+Routes.get("/blogs",getBlogs);
 module.exports = Routes;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { AddUser, login } = require("../controller/user");
+const { getBlogs } = require("../controller/blog");
 
 const routes = express.Router();
 
 // routes.post("/createuser",AddUser);
 routes.post("/login",login);
-
+routes.get("/blogs",getBlogs);
 module.exports = routes;
